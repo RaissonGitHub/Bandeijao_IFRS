4b04c012c08d954aa04f0db539b06232bd410c37
RaissonGitHub <raissonarbustepaludo@gmail.com>
2023-09-05 09:13:32 -0300

    modificacoes no banco

--- models/Cardapio.js
+++ models/Cardapio.js
@@ -13,9 +13,18 @@ module.exports = class Cardapio {
         function obterdia(dia){
             switch (dia){
                 case 1: dia = 'segunda-feira'
+				break;
+                case 2: dia = 'terça-feira'
+				break;
+                case 3: dia = 'quarta-feira'
+				break;
+                case 4: dia = 'quinta-feira'
+				break;
+                case 5: dia = 'sexta-feira'
+				break;
             }
         }
-		connection.query(sql, [dia,tipo], function (err, result) {
+		connection.query(sql, [obterdia(dia),tipo], function (err, result) {
 			if (err) throw err;
 		});
 	}
